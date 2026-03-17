4c5d90fb457b5ea2a34023b852f849c5eaf6adfe
kargamant <firexholms@gmail.com>
2026-03-17 18:53:46 +0300

    Added linker option to disasm to resolve excact addresses and imm values + Makefile updates

--- risc_v/c_programs/basic_disasm.s
+++ risc_v/c_programs/basic_disasm.s
@@ -1,56 +1,56 @@
 
-basic.o:	file format elf32-littleriscv
+bin/basic:	file format elf32-littleriscv
 
 
 Disassembly of section .text:
 
-00000000 <foo>:
-       0: 13 01 01 fe  	addi	sp, sp, -32
-       4: 23 2e 11 00  	sw	ra, 28(sp)
-       8: 23 2c 81 00  	sw	s0, 24(sp)
-       c: 13 04 01 02  	addi	s0, sp, 32
-      10: 23 28 a4 fe  	sw	a0, -16(s0)
-      14: 23 26 b4 fe  	sw	a1, -20(s0)
-      18: 83 25 04 ff  	lw	a1, -16(s0)
-      1c: 03 25 c4 fe  	lw	a0, -20(s0)
-      20: 63 50 b5 00  	bge	a0, a1, 0x20 <foo+0x20>
-      24: 6f 00 00 00  	jal	zero, 0x24 <foo+0x24>
+000110b4 <foo>:
+   110b4: 13 01 01 fe  	addi	sp, sp, -32
+   110b8: 23 2e 11 00  	sw	ra, 28(sp)
+   110bc: 23 2c 81 00  	sw	s0, 24(sp)
+   110c0: 13 04 01 02  	addi	s0, sp, 32
+   110c4: 23 28 a4 fe  	sw	a0, -16(s0)
+   110c8: 23 26 b4 fe  	sw	a1, -20(s0)
+   110cc: 83 25 04 ff  	lw	a1, -16(s0)
+   110d0: 03 25 c4 fe  	lw	a0, -20(s0)
+   110d4: 63 5e b5 00  	bge	a0, a1, 0x110f0 <.LBB0_2>
+   110d8: 6f 00 40 00  	jal	zero, 0x110dc <.LBB0_1>
 
-00000028 <.LBB0_1>:
-      28: 03 25 04 ff  	lw	a0, -16(s0)
-      2c: 83 25 c4 fe  	lw	a1, -20(s0)
-      30: 33 05 b5 00  	add	a0, a0, a1
-      34: 23 2a a4 fe  	sw	a0, -12(s0)
-      38: 6f 00 00 00  	jal	zero, 0x38 <.LBB0_1+0x10>
+000110dc <.LBB0_1>:
+   110dc: 03 25 04 ff  	lw	a0, -16(s0)
+   110e0: 83 25 c4 fe  	lw	a1, -20(s0)
+   110e4: 33 05 b5 00  	add	a0, a0, a1
+   110e8: 23 2a a4 fe  	sw	a0, -12(s0)
+   110ec: 6f 00 80 01  	jal	zero, 0x11104 <.LBB0_3>
 
-0000003c <.LBB0_2>:
-      3c: 03 25 04 ff  	lw	a0, -16(s0)
-      40: 83 25 c4 fe  	lw	a1, -20(s0)
-      44: 33 05 b5 40  	sub	a0, a0, a1
-      48: 23 2a a4 fe  	sw	a0, -12(s0)
-      4c: 6f 00 00 00  	jal	zero, 0x4c <.LBB0_2+0x10>
+000110f0 <.LBB0_2>:
+   110f0: 03 25 04 ff  	lw	a0, -16(s0)
+   110f4: 83 25 c4 fe  	lw	a1, -20(s0)
+   110f8: 33 05 b5 40  	sub	a0, a0, a1
+   110fc: 23 2a a4 fe  	sw	a0, -12(s0)
+   11100: 6f 00 40 00  	jal	zero, 0x11104 <.LBB0_3>
 
-00000050 <.LBB0_3>:
-      50: 03 25 44 ff  	lw	a0, -12(s0)
-      54: 03 24 81 01  	lw	s0, 24(sp)
-      58: 83 20 c1 01  	lw	ra, 28(sp)
-      5c: 13 01 01 02  	addi	sp, sp, 32
-      60: 67 80 00 00  	jalr	zero, 0(ra)
+00011104 <.LBB0_3>:
+   11104: 03 25 44 ff  	lw	a0, -12(s0)
+   11108: 03 24 81 01  	lw	s0, 24(sp)
+   1110c: 83 20 c1 01  	lw	ra, 28(sp)
+   11110: 13 01 01 02  	addi	sp, sp, 32
+   11114: 67 80 00 00  	jalr	zero, 0(ra)
 
-00000064 <main>:
-      64: 13 01 01 ff  	addi	sp, sp, -16
-      68: 23 26 11 00  	sw	ra, 12(sp)
-      6c: 23 24 81 00  	sw	s0, 8(sp)
-      70: 13 04 01 01  	addi	s0, sp, 16
-      74: 13 05 00 00  	addi	a0, zero, 0
-      78: 23 28 a4 fe  	sw	a0, -16(s0)
-      7c: 23 2a a4 fe  	sw	a0, -12(s0)
-      80: 13 05 10 00  	addi	a0, zero, 1
-      84: 93 05 20 00  	addi	a1, zero, 2
-      88: 97 00 00 00  	auipc	ra, 0
-      8c: e7 80 00 00  	jalr	ra, 0(ra)
-      90: 03 25 04 ff  	lw	a0, -16(s0)
-      94: 03 24 81 00  	lw	s0, 8(sp)
-      98: 83 20 c1 00  	lw	ra, 12(sp)
-      9c: 13 01 01 01  	addi	sp, sp, 16
-      a0: 67 80 00 00  	jalr	zero, 0(ra)
+00011118 <main>:
+   11118: 13 01 01 ff  	addi	sp, sp, -16
+   1111c: 23 26 11 00  	sw	ra, 12(sp)
+   11120: 23 24 81 00  	sw	s0, 8(sp)
+   11124: 13 04 01 01  	addi	s0, sp, 16
+   11128: 13 05 00 00  	addi	a0, zero, 0
+   1112c: 23 28 a4 fe  	sw	a0, -16(s0)
+   11130: 23 2a a4 fe  	sw	a0, -12(s0)
+   11134: 13 05 10 00  	addi	a0, zero, 1
+   11138: 93 05 20 00  	addi	a1, zero, 2
+   1113c: 97 00 00 00  	auipc	ra, 0
+   11140: e7 80 80 f7  	jalr	ra, -136(ra)
+   11144: 03 25 04 ff  	lw	a0, -16(s0)
+   11148: 03 24 81 00  	lw	s0, 8(sp)
+   1114c: 83 20 c1 00  	lw	ra, 12(sp)
+   11150: 13 01 01 01  	addi	sp, sp, 16
+   11154: 67 80 00 00  	jalr	zero, 0(ra)
